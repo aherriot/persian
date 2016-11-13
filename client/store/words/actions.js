a3db72569c6be93f8b2b7ab83081590aaeb6427c
Andrew Herriot <agherriot@gmail.com>
2016-11-13 12:40:12 -0500

    Fixing api request routes

--- client/store/words/actions.js
+++ client/store/words/actions.js
@@ -1,21 +1,21 @@
 import {authRequest} from 'utils/request';
 
 export function fetchWords() {
-  return authRequest('FETCH_WORDS', 'api/words');
+  return authRequest('FETCH_WORDS', '/api/words');
 }
 
 export function addWord(word) {
-  return authRequest('ADD_WORD', 'api/words', 'POST', word);
+  return authRequest('ADD_WORD', '/api/words', 'POST', word);
 }
 
 export function bulkAddWords(words) {
-  return authRequest('BULK_ADD_WORDS', 'api/words', 'POST', words);
+  return authRequest('BULK_ADD_WORDS', '/api/words', 'POST', words);
 }
 
 export function deleteWord(word) {
-  return authRequest('DELETE_WORD', 'api/words/' + word._id, 'DELETE');
+  return authRequest('DELETE_WORD', '/api/words/' + word._id, 'DELETE');
 }
 
 export function editWord(word) {
-  return authRequest('EDIT_WORD', 'api/words/' + word._id, 'PUT', {word: word});
+  return authRequest('EDIT_WORD', '/api/words/' + word._id, 'PUT', {word: word});
 }
