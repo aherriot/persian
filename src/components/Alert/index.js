9ff901b5b8c451cf20f82173a34fcb4b48d1ea9d
Andrew Herriot <agherriot@gmail.com>
2017-11-12 21:41:30 -0500

    Cleaned up profile page

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -17,15 +17,34 @@ class Alert extends Component {
     }
   }
 
-  onClose = () => {
+  onAffirm = () => {
     const { alerts } = this.props
 
     this.setState({ open: false })
 
     this.timerRef = window.setTimeout(() => {
+      // This code can definitely be improved
+      // The problem here is that we want to push a queue
+      // of alert messages into the redux state,
+      // but we can't (shouldn't) put callbacks
+      // or non-serializable data in the redux state.
+
+      // Instead, I have put an action type for each alert,
+      // and that allows me to perform different tasks
+      // when the form is dismissed. This should be reworked
+      // into a better solution, but maybe on a rainy day.
+
       if (alerts[0].action === 'reload') {
         window.location.reload()
       }
+
+      this.props.actions.hideAlert()
+    }, 400)
+  }
+
+  onReject = () => {
+    this.setState({ open: false })
+    this.timerRef = window.setTimeout(() => {
       this.props.actions.hideAlert()
     }, 400)
   }
@@ -48,7 +67,12 @@ class Alert extends Component {
         <div className="form">
           <div className="form__body">{alert.text}</div>
           <div className="form__button-row">
-            <button type="button" className="button" onClick={this.onClose}>
+            {alert.action === 'reload' && (
+              <button type="button" className="button" onClick={this.onReject}>
+                No
+              </button>
+            )}
+            <button type="button" className="button" onClick={this.onAffirm}>
               Okay
             </button>
           </div>
