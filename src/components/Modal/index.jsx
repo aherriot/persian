662caee4613b334673e39b1d74f006a1494ffffa
Andrew Herriot <agherriot@gmail.com>
2017-11-12 20:46:02 -0500

    showing not authorized message on add new word modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ReactModal from 'react-modal'
 
@@ -16,18 +16,46 @@ const overlayClassNames = {
   beforeClose: 'Modal__overlay--before-close'
 }
 
-export default function Modal({ open, onClose, title, children }) {
-  return (
-    <ReactModal
-      isOpen={open}
-      onRequestClose={onClose}
-      closeTimeoutMS={200}
-      className={windowClassNames}
-      overlayClassName={overlayClassNames}>
-      <h1>{title}</h1>
-      {children}
-    </ReactModal>
-  )
+export default class Modal extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { open: false }
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (this.props.open !== newProps.open)
+      if (newProps.open) {
+        this.setState({ open: true })
+      } else {
+        this.setState({ open: false })
+      }
+  }
+
+  componentWillUnmount() {
+    window.clearTimeout(this.timeoutRef)
+  }
+
+  onRequestClose = () => {
+    this.setState({ open: false })
+
+    this.timeoutRef = window.setTimeout(this.props.onClose, 200)
+  }
+
+  render() {
+    const { title, children } = this.props
+    return (
+      <ReactModal
+        isOpen={this.state.open}
+        onRequestClose={this.onRequestClose}
+        closeTimeoutMS={200}
+        className={windowClassNames}
+        overlayClassName={overlayClassNames}>
+        <h1>{title}</h1>
+        {children}
+      </ReactModal>
+    )
+  }
 }
 
 Modal.propTypes = {
