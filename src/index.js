d60899dce185f4bbca3a6cda46e3a88960e85ef7
Andrew Herriot <agherriot@gmail.com>
2017-12-02 17:05:19 -0500

    Adding google analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom'
 import store, { history } from './store/store'
+import ReactGA from 'react-ga'
 
 import Routes from './routes/Routes'
 import registerServiceWorker from './registerServiceWorker'
@@ -15,4 +16,10 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
+
 registerServiceWorker()
+
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('UA-52203908-2')
+  ReactGA.pageview(window.location.pathname + window.location.search)
+}
